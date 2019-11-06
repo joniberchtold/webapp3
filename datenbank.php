ea5b74aa5d45451fd3648b52ae62d759b1e3e0cc
joniberchtold <54633221+joniberchtold@users.noreply.github.com>
2019-11-06 16:55:29 +0100

    ausgabe

---
+++ datenbank.php
@@ -0,0 +1,3 @@
+<?php
+$pdo = new PDO('mysql:host=localhost;dbname=online_shop', 'root', '');
+?>
--- shop.php
+++ shop.php
@@ -11,21 +11,31 @@
    <?php
         // Hier werden die DB verlinkung und die NAvigation eingefügt
         include "navigation.php";
+		include "datenbank.php";
        
 
 ?>
-
-  <p>Jonas</p>
-  <p>Jonas</p>
-  <p>Jonas</p>
-  <p>Jonas</p>
-  <p>Jonas</p>
+	<?php
+	//Ausgabe Datenbank Produkte
+	
+        $sql = "SELECT ID, Name, Artikelnummer, Bild, Beschreibung FROM artikel";
+        foreach ($pdo->query($sql) as $row) {
+                $produkt_id = $row['ID'];
+				$produkt_name = $row['Name'];
+				$produkt_artikelnummer = $row['Artikelnummer'];
+                $produkt_bild = $row['Bild'];
+                $produkt_beschreibung = $row['Beschreibung'];
+        ?>
+            <div id="produkt">
+            <div id="produkt_titel"><h1><?php echo $produkt_name; ?></h1></div>
+            <div id="produkt_bild"><img src="bilder/<?php echo $produkt_bild; ?>" /></div>
+			<div id="produkt_nummer"><?php echo $produkt_artikelnummer; ?>"</div>
+            <div id="produkt_beschreibung"><?php echo $produkt_beschreibung; ?></div>
+            <div id="produkt_bearbeitung"><a href="edit.php?eid=<?php echo $produkt_id; ?>">Bearbeiten</a> - <a href="del.php?did=<?php echo $produkt_id; ?>">Löschen</a></div>
+            </div>
+        <?php
+        }
+        ?>
   
-   <?php
-        // Hier werden die DB verlinkung und die NAvigation eingefügt
-        include "navunten.php";
-       
-
-?>
   </body>
 </html>
